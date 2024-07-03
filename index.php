2d87f9602985ce69eeca94c7a26bebf2dd9782da
Thaynara Sabryna <112216179+thaynarasabryna@users.noreply.github.com>
2024-07-03 15:41:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Cadastro</title>
+    <title>Empresa</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Cadastro</h1>
-                <form action="action.php" method="post">
-                    <div class="form-group mb-3">
-                        <label class="mb-2" for="nome">Nome completo</label>
-                        <input type="text" class="form-control" if="nome" name="nome">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label class="mb-2" for="nome">Endereço</label>
-                        <input type="text" class="form-control" if="cep" name="cep">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label class="mb-2" for="nome">Telefone</label>
-                        <input type="text" class="form-control" if="telefone" name="telefone">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label class="mb-2" for="nome">Email</label>
-                        <input type="email" class="form-control" if="email" name="email">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label class="mb-2" for="nome">Data de Nascimento</label>
-                        <input type="date" class="form-control" if="data_nascimento" name="data_nascimento">
-                    </div>
-                    <div class="form-group mb-3">
-                        <input type="submit" class="btn btn-success">
-                </form>
+                <div class="jumbotron bg-light p-5">
+                    <h1 class="display-4">Cadastro Web</h1>
+                    <p class="lead">Este é um sistema simplificado de cadastros. Base de estudos para criação de sistemas Web com PHP e MySQL.</p>
+                    <hr class="my-4">
+                    <p>Acesse as funções.</p>
+                    <a class="btn btn-primary btn-lg" href="cadastrar.php" role="button">Cadastro</a>
+                    <a class="btn btn-primary btn-lg" href="listar.php" role="button">Pesquisar</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html> 
