3e0f6d7ab04f5169b6adc2bb30c368fbcb66364a
Thaynara Sabryna <112216179+thaynarasabryna@users.noreply.github.com>
2024-07-05 17:06:53 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,5 +1,9 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,39 +18,37 @@
                 <form action="actions.php" method="post">
                     <div class="form-group mb-3">
                         <label class="mb-2" for="nome">Nome completo</label>
-                        <input type="text" class="form-control" id="nome" name="nome">
+                        <input type="text" class="form-control" if="nome" name="nome">
                     </div>
                     <div class="form-group mb-3">
-                        <label class="mb-2" for="endereco">Endereço</label>
-                        <input type="text" class="form-control" id="endereco" name="endereco">
+                        <label class="mb-2" for="nome">Endereço</label>
+                        <input type="text" class="form-control" if="cep" name="cep">
                     </div>
                     <div class="form-group mb-3">
-                        <label class="mb-2" for="telefone">Telefone</label>
-                        <input type="text" class="form-control" id="telefone" name="telefone">
+                        <label class="mb-2" for="nome">Telefone</label>
+                        <input type="text" class="form-control" if="telefone" name="telefone">
                     </div>
                     <div class="form-group mb-3">
-                        <label class="mb-2" for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email">
+                        <label class="mb-2" for="nome">Email</label>
+                        <input type="email" class="form-control" if="email" name="email">
                     </div>
                     <div class="form-group mb-3">
-                        <label class="mb-2" for="data_nascimento">Data de Nascimento</label>
-                        <input type="date" class="form-control" id="data_nascimento" name="data_nascimento">
+                        <label class="mb-2" for="nome">Data de Nascimento</label>
+                        <input type="date" class="form-control" if="data_nascimento" name="data_nascimento">
                     </div>
                     <div class="form-group mb-3">
                         <input type="submit" class="btn btn-success">
                     </div>
                 </form>
                 <a href="index.php" class="btn btn-primary text-white">Voltar para o Início</a>
-                <?php 
-                    if (mysqli_query($conn, $sql)) {
-                        mensagem("$nome cadastrado com sucesso!", 'success');
-                    } else {
-                        mensagem("$nome NAO foi cadastrado.", 'danger');
-                    }  
-                ?>
+
+                <div class="alert alert-<?php echo $alerta_cadastro; ?>" role="alert" id="alerta">
+                    <?php echo $_SESSION['alerta']; ?>
+                </div>
+                <?php unset($_SESSION['alerta']); ?>
             </div>
         </div>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-</html>
+</html> 
