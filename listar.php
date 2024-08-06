7ce957216c412fde2f6594dda4031565e7e7fb16
Thaynara Sabryna <112216179+thaynarasabryna@users.noreply.github.com>
2024-08-06 11:02:14 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -4,65 +4,125 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="./js/script.js"></script>
     <title>Pesquisar</title>
 </head>
 <body>
-    <?php
-        $pesquisa = $_POST['busca'] ?? '';
-        include "conexao.php";
 
-        $sql = "SELECT * FROM pessoas WHERE nome LIKE '%$pesquisa%'";
+    <?php
+        include "conexao.php";
+        
+        session_start();
+
+        $mensagem = $_SESSION['mensagem'];
+        $mensagem_tipo = $_SESSION['mensagem_tipo'];
+        unset($_SESSION['mensagem']);
+        unset($_SESSION['mensagem_tipo']);
+
+        if (!$conn) {
+            die("Falha na conexão: " . mysqli_connect_error());
+        }
+
+        if (isset($_POST['busca'])) {
+            $pesquisa = $_POST['busca'];
+        } else {
+            $pesquisa = '';
+        }
+
+        $sql = "SELECT
+                    p.id,
+                    p.nome, 
+                    p.email, 
+                    p.cpf, 
+                    p.data_nascimento,
+                    e.cep,
+                    e.logradouro,
+                    e.numero,
+                    e.complemento,
+                    e.bairro,
+                    e.cidade,
+                    e.uf
+                FROM pessoas p 
+                LEFT JOIN enderecos e ON p.id = e.id_pessoas
+                WHERE nome LIKE '%$pesquisa%'";
+
         $dados = mysqli_query($conn, $sql);
     ?>
-
-    
-
-    <div class="container">
-        <div class="row">
-            <div class="col mt-5">
-                <h1>Pesquisar</h1>
-                <nav class="navbar navbar-light bg-light">
-                    <form class="form-inline d-flex" action="listar.php" method="POST">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Nome" aria-label="Search" name="busca" autofocus>
+    <div class="container-fluid mt-5">
+        <div class="card shadow-sm mb-4 col-lg-12">
+            <div class="card-header bg-primary text-uppercase text-white text-center">Pesquisar Registros</div>
+            <div class="card-body">
+                <nav class="navbar navbar-light bg-light mb-3">
+                    <form class="form-inline d-flex w-10" action="pesquisa.php" method="POST">
+                        <input class="form-control mr-sm-2 w-75" type="search" placeholder="Pesquisar" aria-label="Pesquisar" name="busca" autofocus>
                         <button class="btn btn-outline-success my-2 my-sm-0 m-2" type="submit">Pesquisar</button>
                     </form>
                 </nav>
                 <table class="table table-hover">
                     <thead>
                         <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Endereço</th>
-                        <th scope="col">Telefone</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Data de Nascimento</th>
+                            <th scope="col" class="small">Nome</th>
+                            <th scope="col" class="small">Email</th>
+                            <th scope="col" class="small">CPF</th>
+                            <th scope="col" class="small">Data de Nascimento</th>
+                            <th scope="col" class="small">CEP</th>
+                            <th scope="col" class="small">Logradouro</th>
+                            <th scope="col" class="small">Número</th>
+                            <th scope="col" class="small">Complemento</th>
+                            <th scope="col" class="small">Bairro</th>
+                            <th scope="col" class="small">Cidade</th>
+                            <th scope="col" class="small">UF</th>
+                            <th scope="col" class="small">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
-                    <?php
-                        while($linha = mysqli_fetch_assoc($dados)) {
-                            $cod_pessoa = $linha['cod_pessoa'];
-                            $nome = $linha['nome'];
-                            $endereco = $linha['endereco'];
-                            $telefone = $linha['telefone'];
-                            $email = $linha['email'];
-                            $data_nascimento = $linha['data_nascimento'];
-                            
-                            echo "<tr>
-                                    <th scope='row'>$nome</th>
-                                    <td>$endereco</td>
-                                    <td>$telefone</td>
-                                    <td>$email</td>
-                                    <td>$data_nascimento</td>
-                                </tr>";
-                        }
-                    ?>
+                        <?php
                         
+                        if (mysqli_num_rows($dados) > 0) {
+                            while ($linha = mysqli_fetch_assoc($dados)) { ?>
+                                <tr>
+                                    <th scope="row" class="small"><?php echo $linha['nome']?></th>
+                                    <td scope="row" class="small"><?php echo $linha['email']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['cpf']?></td>
+                                    <td scope="row" class="small"><?php echo mostraData($linha['data_nascimento'])?></td>
+                                    <td scope="row" class="small"><?php echo $linha['cep']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['logradouro']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['numero']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['complemento']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['bairro']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['cidade']?></td>
+                                    <td scope="row" class="small"><?php echo $linha['uf']?></td>
+
+                                    <td width="150px">
+                                        <form action="actions.php" method="POST">
+                                            <input type="submit" class="btn btn-primary btn-sm me-1" value="Editar">    
+                                            <a href="#" class="btn btn-danger btn-sm" onclick="carregaModal(<?php echo $linha['id']; ?>, '<?php echo $linha['nome']; ?>')">Excluir</a>
+                                            <input type="hidden" name="id" value="<?php echo $linha['id']?>">
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php }
+                        } else { ?>
+                            <tr>
+                                <td colspan='12' class='text-center'>
+                                    <?php echo "Nenhum registro encontrado"; ?>
+                                </td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
-                <a href="index.php" class="btn btn-primary text-white">Voltar para o Início</a>
             </div>
         </div>
+        <a href="index.php" class="btn btn-primary text-white mb-4">Voltar para o Início</a>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <?php if ($mensagem): ?>
+        <div class="alert alert-<?php echo $mensagem_tipo; ?> alert-dismissible fade show w-50 m-3" role="alert">
+            <?php echo $mensagem; ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    <?php endif; ?>
+    <div id="modalContainer"></div>
 </body>
-</html> 
+</html>
