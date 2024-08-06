578c692b9dff9a5d1653315ab04aef43747af3f4
Thaynara Sabryna <112216179+thaynarasabryna@users.noreply.github.com>
2024-08-06 11:01:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Empresa</title>
+    <title>Página Inicial</title>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col">
+    <div class="container mt-5">
+        <div class="card shadow-sm mb-4">
+            <div class="card-header bg-primary text-uppercase text-white text-center">Bem-vindo ao Sistema de Cadastro</div>
+            <div class="card-body">
                 <div class="jumbotron bg-light p-5">
                     <h1 class="display-4">Cadastro Web</h1>
-                    <p class="lead">Este é um sistema simplificado de cadastros. Base de estudos para criação de sistemas Web com PHP e MySQL.</p>
+                    <p class="lead">Este é um sistema simplificado de cadastros.<br>Base de estudos com PHP e MySQL.</p>
                     <hr class="my-4">
                     <p>Acesse as funções.</p>
-                    <a class="btn btn-primary btn-lg" href="cadastrar.php" role="button">Cadastro</a>
+                    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
                     <a class="btn btn-primary btn-lg" href="listar.php" role="button">Pesquisar</a>
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-</html> 
+</html>
